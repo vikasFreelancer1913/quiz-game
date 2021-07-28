275e97b0493fad32bcf6f9742bc69b9db37fb885
vikas singh <vicky13vikas@gmail.com>
2021-07-28 21:33:11 +0530

    created quiz game

---
+++ script.js
@@ -0,0 +1,269 @@
+const myQuest = {
+	'GK': [
+		{
+			"q": "Serum Institute of India in collaboration with “Gavi” and which other organization has aimed to produce 100 million additional doses of Covid-19 vaccine?",
+			"a": "Bill and Melinda Gates Foundation"
+		},
+		{
+			"q": "Which animal never drinks water in its entire life?",
+			"a": "Kangaroo rat"
+		},
+		{
+			"q": "Guess the personality duo from this Picture?",
+			"a": "The Wright Brothers",
+			"img": "https://i.imgur.com/TplXvyJ.png"
+		},
+		{
+			"q": "Name the common preservative used in pickles and jam.",
+			"a": "Sodium benzoate"
+		},
+		{
+			"q": "Name the three mediums through which sound can travel.",
+			"a": "Solid, liquid, and gas"
+		},
+		{
+			"q": "Three men are on a boat. The boat sinks but only two people get their hair wet. Why?",
+			"a": "The third man was Bald.",
+			"img": "https://i.imgur.com/nPOF7JQ.jpeg"
+		},
+		{
+			"q": "Who built the Jama Masjid?",
+			"a": "Shah Jahan"
+		},
+		{
+			"q": "Guess the personality from the picture, she was the first Indian to win the Booker Prize?",
+			"a": "Arundhati Roy",
+			"img": "https://i.imgur.com/o9I0TF1.png"
+		},
+		{
+			"q": "Who wrote the Indian National Anthem?",
+			"a": "Late Rabindranath Tagore"
+		},
+		{
+			"q": "Who was the first Indian Scientist to win a Nobel Prize?",
+			"a": "Late CV Raman"
+		}
+	],
+	'MT': [
+		{
+			"q": "For how many days was the Battle of Kurukshetra fought?",
+			"a": "18 Days"
+		},
+		{
+			"q": "Which mythological story of Hinduism this picture tells about Lord Vishnu's Avatar?",
+			"a": "Manu and the Matsya avatar",
+			"img": "https://i.imgur.com/YFyM05Y.jpeg"
+		},
+		{
+			"q": "What was the original name of Indrajit - Ramayana?",
+			"a": "Meghanada"
+		},
+		{
+			"q": "What is the major difference between a bird and a fly?",
+			"a": "A bird can fly but a fly cannot bird!",
+			"img": "https://i.imgur.com/3F472QJ.jpeg"
+		},
+		{
+			"q": "Who did Ekalavya consider as his Guru?",
+			"a": "Dronacharya"
+		},
+		{
+			"q": "Who wrote down the epic Mahabharata while Vyasa was dictating?",
+			"a": "Lord Ganesh"
+		},
+		{
+			"q": "Tell the name of Vishnu Avatar and the Demon in the Picture?",
+			"a": "Narshima avatar and Hiranyakashipu",
+			"img": 'https://i.imgur.com/lZoLBC4.jpeg'
+		},
+		{
+			"q": "Which one is the first avatar of Lord Vishnu from his Dashavatar?",
+			"a": "Matsya"
+		},
+		{
+			"q": "In which Yuga, Shree Rama was born in Ayodhya?",
+			"a": "Treta Yuga"
+		},
+		{
+			"q": "Sati (wife of Lord Shiva) was the daughter of which King?",
+			"a": "Daksha Prajapati"
+		}
+	],
+	'SP': [
+		{
+			"q": "What is the duration of a Test Match?",
+			"a": "5 days"
+		},
+		{
+			"q": "Guess the personality, he is the most medal winner in Olympics till date?",
+			"a": "Michael Phelps - 28",
+			"img": "https://i.imgur.com/kUxJAvW.jpeg"
+		},
+		{
+			"q": "Which country is the “country of origin” of taekwondo?",
+			"a": "Korea"
+		},
+		{
+			"q": "Which is the most successful team in Men’s Hockey World Cup championship?",
+			"a": "Pakistan"
+		},
+		{
+			"q": "How many number of players one team of Kho-Kho has?",
+			"a": "9"
+		},
+		{
+			"q": "Why do some cricket players never sweat?",
+			"a": "Because they have huge fans.",
+			"img": "https://i.imgur.com/J47aATj.jpeg"
+		},
+		{
+			"q": "Ghulam Ahmed Trophy belongs to which sports?",
+			"a": "Cricket"
+		},
+		{
+			"q": "Who has won Australian open men’s singles 2017?",
+			"a": "Roger Federer"
+		},
+		{
+			"q": "This man in the picture had made history this pic is from same day, what was the achievement and against which team?",
+			"a": "First cricketer to make 200 runs in ODI against South Africa",
+			"img": "https://i.imgur.com/U2chpiG.jpeg"
+		},
+		{
+			"q": "First Asian Games held in which nation?",
+			"a": "India in 1951"
+		}
+	],
+	'ENT': [
+		{
+			"q": "Take a look to the picture and tell the Movie name!",
+			"a": "The Matrix Reloaded",
+			"img": "https://i.imgur.com/QyHTAM9.jpeg"
+		},
+		{
+			"q": "By looking at the picture tell this particular scene Dialogue and the name of the Movie?",
+			"a": "'Poora naam, Vijay Dinanath Chauhan' - Agnipath",
+			"img": "https://i.imgur.com/ob3ds88.jpeg"
+		},
+		{
+			"q": "What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?",
+			"a": "2008",
+			"img": "https://i.imgur.com/tMbGKG3.jpeg"
+		},
+		{
+			"q": "If you’re a true K-serial fan, you must know her name.",
+			"a": "Komolika!",
+			"img": "https://i.imgur.com/WiFw5fe.jpeg"
+		},
+		{
+			"q": "Take a look to these 2 characters and tell the name of this T.V Serial!",
+			"a": "Sarabhai vs Sarabhai",
+			"img": "https://i.imgur.com/DIwkvKN.jpeg"
+		},
+		{
+			"q": "If a plane crashes on the border between the United States and Canada, where do they bury the survivors?",
+			"a": "Survivors are not buried.",
+			"img": "https://i.imgur.com/NRenLbK.jpeg"
+		},
+		{
+			"q": "Tell the name of Vishnu Avatar and the Demon in this Picture?",
+			"a": "Narshima avatar and Hiranyakashipu",
+			"img": 'https://i.imgur.com/lZoLBC4.jpeg'
+		},
+		{
+			"q": "Who wrote malgudi days?",
+			"a": "R. K. Narayan",
+			"img": "https://i.imgur.com/avDnpLN.jpeg"
+		},
+		{
+			"q": "In all the Marvel movies who does the special Cameo appearances?",
+			"a": "Stan Lee",
+			"img": "https://i.imgur.com/azbP8Og.jpeg"
+		},
+		{
+			"q": "DDLJ, the most loved movie of Indian cinema. What is the name of the character of Amirsh Puri in this Movie?",
+			"a": "Chaudhry Baldev Singh",
+			"img": "https://i.imgur.com/DQD3Dxy.jpeg"
+		}
+	]
+};
+
+let GKIndex = 0;
+let MTIndex = 0;
+let SPIndex = 0;
+let ENTIndex = 0;
+
+$(window).on('load', () => {
+	setTimeout(() => {
+		$('.js-preLoader').hide();
+		$('.js-quizOuterWrapper').show();
+	}, 6000);
+});
+
+$('.queryBtn').on('click', (event) => {
+	event.stopPropagation();
+	$('button').removeClass('active');
+	$(event.currentTarget).addClass('active');
+	
+	resetDivs();
+	
+	const qVar = $(event.currentTarget).attr('id').split('-')[1];
+	let randomIndex = 0;
+	switch(qVar) {
+		case 'GK' : randomIndex = GKIndex;
+								GKIndex++;
+								break;
+		case 'MT' : randomIndex = MTIndex;
+								MTIndex++;
+								break;
+		case 'SP' : randomIndex = SPIndex;
+								SPIndex++;
+								break;
+		case 'ENT': randomIndex = ENTIndex;
+								ENTIndex++;
+								break;
+		default: 		randomIndex = 0;
+								break;
+	}
+	
+	if (GKIndex === 10) {
+		$('#js-GK').attr("disabled", true).removeClass('active').addClass('disabled');
+	}
+	if (MTIndex === 10) {
+		$('#js-ST').attr("disabled", true).removeClass('active').addClass('disabled');
+	}
+	if (SPIndex === 10) {
+		$('#js-SP').attr("disabled", true).removeClass('active').addClass('disabled');
+	}
+	if (ENTIndex === 10) {
+		$('#js-ENT').attr("disabled", true).removeClass('active').addClass('disabled');
+	}
+
+	$('#js-qSection').find('.query').html('Q: ' + myQuest[qVar][randomIndex].q);
+	if (myQuest[qVar][randomIndex]?.img) {
+		$("#js-qSection").find('#js-queryImg').attr('src', myQuest[qVar][randomIndex].img);
+		$('.js-imgWrap').show();
+	}
+	
+	$('#js-qSection').show();
+	$('.js-showAnswer').on('click', (event) => {
+		event.stopPropagation();
+		$(event.currentTarget).hide();
+		$("#js-aSection").find('.answer').html('A: ' + myQuest[qVar][randomIndex].a);
+		$("#js-aSection").show();
+	});
+	
+	$('.js-clearAnswer').on('click', (event) => {
+		event.stopPropagation();
+		resetDivs();
+	});
+});
+
+function resetDivs() {
+	$('.js-showAnswer').show();
+	$("#js-aSection").find('.answer').html('');
+	$("#js-aSection").hide();
+	$('#js-qSection').find('.query').html('');
+	$('#js-qSection').hide();
+	$('.js-imgWrap').hide();
+}
